6b27a9dfc700ee324a50d4a2a8aac9b0400afe2a
SekllanChenRuan <xirenchen94@gmail.com>
2023-09-24 17:26:09 -0700

    remove background image, update bootstrap MDN to version 5.3.2, add dark mode functionality

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const modeToggle = document.querySelector(".modetoggle");
+
+if (localStorage.getItem("mode") === "dark") {
+  body.setAttribute("data-bs-theme", "dark");
+} else {
+  body.setAttribute("data-bs-theme", "light");
+}
+
+if (localStorage.getItem("icon-mode") === "fa-moon-o") {
+  modeToggle.classList.remove("fa-moon-o");
+  modeToggle.classList.add("fa-sun-o");
+} else {
+  modeToggle.classList.remove("fa-sun-o");
+  modeToggle.classList.add("fa-moon-o");
+}
+
+function toggleMode() {
+  if (modeToggle.classList.contains("fa-moon-o")) {
+    body.setAttribute("data-bs-theme", "dark");
+    modeToggle.classList.remove("fa-moon-o");
+    modeToggle.classList.add("fa-sun-o");
+    localStorage.setItem("icon-mode", "fa-moon-o");
+    localStorage.setItem("mode", "dark");
+  } else {
+    body.setAttribute("data-bs-theme", "light");
+    modeToggle.classList.remove("fa-sun-o");
+    modeToggle.classList.add("fa-moon-o");
+    localStorage.setItem("icon-mode", "fa-sun-o");
+    localStorage.setItem("mode", "light");
+  }
+}
