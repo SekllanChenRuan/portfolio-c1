ad308d8efd720acbe16ce29b6ce8f0dbfbfe5880
SekllanChenRuan <xirenchen94@gmail.com>
2023-10-17 09:34:04 -0700

    removed some folders to clean up project

--- grateful-graham/astro.config.mjs
+++ grateful-graham/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https:///SekllanChenRuan.github.io/",
+  site: "https:///SekllanChenRuan.github.io",
   base: "/portfolio-c1/grateful-graham",
 });
